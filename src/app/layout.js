1e3161d249ca5b85a3ba2f378a213f1c48d07ae3
laurenciopaez <laurencio314gmail.com>
2023-12-26 23:08:47 -0300

    set up

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,7 @@
 import { Inter } from 'next/font/google'
 import './globals.css'
+import { NextUIProvider } from '@nextui-org/react'
+import { ThemeProvider } from 'next-themes'
 
 const inter = Inter({ subsets: ['latin'] })
 
---
+++ src/components/LandingComponent.jsx
@@ -0,0 +1,11 @@
+'use client'
+
+const LandingPage = () => {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default LandingPage;
