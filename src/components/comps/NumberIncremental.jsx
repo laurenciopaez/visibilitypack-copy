dd0457f6fbc8e3677ab6ec75e5e06eda8ed90f53
laurenciopaez <laurencio314gmail.com>
2024-01-01 12:30:16 -0300

    last fix

--- src/components/comps/NumberIncremental.jsx
+++ src/components/comps/NumberIncremental.jsx
@@ -36,7 +36,7 @@ const NumberIncremental = ({targetNumber, text, color}) => {
 
     return (
         <div style={{color:`${color}`}} ref={ref} className="shadow-lg border-2 rounded-xl py-2 px-4 w-1/2">
-            <h1 className="flex justify-center content-center mb-2">{currentNumber}+</h1>
+            <h1 className="flex justify-center content-center mb-2 text-xl">{currentNumber}+</h1>
             <p className="flex justify-center text-lg text-black ">{text}</p>
         </div>
     )
